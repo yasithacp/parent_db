5cce238fe4f6c78cec5116c7f45ebd787a1efc42
yasithacp <yasitha4@gmail.com>
2014-07-20 00:33:09 +0530

    Intial UI changes and parent plugin creation

---
+++ symfony/plugins/orangehrmParentPlugin/modules/parent/actions/addParentInfoAction.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: yasitha
+ * Date: 7/20/14
+ * Time: 12:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class addParentInfoAction extends sfAction {
+
+    public function execute($request) {
+
+    }
+}
--- symfony/plugins/orangehrmTimePlugin/modules/time/templates/_employeeReportHeader.php
+++
@@ -1,3 +0,0 @@
-<table>
-     <tr><td style="width: 100px;"><?php echo " ".__("Employee Name")."  ";?></td><td><?php echo $empName;?></td></tr>
-</table>
